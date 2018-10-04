353aa77bdcab6adbc0c96c8c6fc3ed4cfa052980
Ricardo Paulo Da Cunha <50473694808@info.local>
2018-10-04 11:32:19 -0300

    Arquivos 04/10/2018

---
+++ Senai.Lacos.Repeticao/Senai.Lacos.Repeticao.Exercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Senai.Lacos.Repiticao.Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Informa os números entre dois outros números
+            Console.WriteLine("Informe o Primeiro número da sequência:");
+            int NumeroInicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Ultimo número da sequência");
+            int NumeroChegada = int.Parse(Console.ReadLine());
+
+            int Contador = NumeroInicial;
+            Console.Clear();
+            if(NumeroInicial < NumeroChegada){
+                while (Contador < NumeroChegada -1){
+                    Contador++;
+                    Console.WriteLine(Contador);
+                }
+            }else{
+                while (Contador > NumeroChegada +1){
+                    Contador--;
+                    Console.WriteLine(Contador);
+                }
+            }
+        }
+    }
+}
---
+++ Senai.Lacos.Repeticao/Senai.Lacos.Repeticao.Exercicio2 - Copia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Senai.Lacos.Repeticao.Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastre uma Senha:");
+            string SenhaCorr = Console.ReadLine();
+
+            Console.WriteLine("Por favor insera a senha novamente:");
+            string SenhaInco = "invalido";
+
+            while (SenhaCorr != SenhaInco)
+            {
+                Console.WriteLine("Por favor insera a senha novamente:");
+                SenhaInco = Console.ReadLine();
+            }
+
+            if(SenhaCorr == SenhaInco){
+                Console.WriteLine("Senha correta");
+            }else{
+
+            }
+        }
+    }
+}

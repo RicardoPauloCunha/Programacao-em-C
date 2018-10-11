84c71555325ef023c1bda4f23124fab481b90a46
Ricardo Paulo Da Cunha <50473694808@info.local>
2018-10-11 10:56:19 -0300

    Arquivos 11/10/2018

---
+++ Senai.Array/Senai.Array.Exercicio3.MediaAritmetica/Classes/MediaAritmetica.cs
@@ -0,0 +1,8 @@
+namespace Senai.Array.Exercicio3.MediaAritmetica.Classes
+{
+    public class MediaAritmetica
+    {
+        public int[] numero = new int[5];
+        public int contador = 0;
+    }
+}
---
+++ Senai.Exemplos/Senai.Array.Exemplo1.Bingo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Senai.Array.Exemplo1.Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] cartela = new int[6]{9, 24, 36, 41, 57, 66};
+            int acertos = 0, posicao, numero;
+
+            do {
+                Console.WriteLine("Informe um numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                posicao = System.Array.IndexOf(cartela, numero);
+
+                if (posicao >= 0) {
+                    acertos++; 
+                    Console.WriteLine($"Acertou {acertos}, falta {6 - acertos}");
+                }
+            } while (acertos < 6);
+
+            Console.WriteLine("Parabéns. Você ganhou!!!");
+        }
+    }
+}

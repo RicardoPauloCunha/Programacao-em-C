978028dc151fe696ab6c0967ec2eadd3016394ba
Ricardo Paulo Da Cunha <50473694808@info.local>
2018-10-11 11:24:15 -0300

    Arquivos 11/10/2018

---
+++ Senai.Exemplos/Senai.For.Exemplo2.AlgoritmoExibirNaN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Senai.For.Exemplo2.AlgoritmoExibirNaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int cont = 200; cont >= 0; cont--)
+            {
+                Console.WriteLine(cont);
+            }
+        }
+    }
+}
